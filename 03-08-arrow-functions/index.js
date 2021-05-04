62b488ef2dff412b5a93766f1bfec04944725676
RobertoScolar <robertobs1@hotmail.com>
2021-05-04 13:49:30 -0300

    Arrow Functions em Javascript

---
+++ 03-08-arrow-functions/index.js
@@ -0,0 +1,22 @@
+//function declaration
+
+function name () {
+
+}
+
+//function expression
+
+const nome = function() {
+
+}
+
+//arrow function
+//funciona do mesmo que uma função mas a sintaxe muda um pouco
+
+//const getRemainingYears = (age) => { 
+//  return 18 - age;
+//}
+
+const getRemainingYears = (age) => 18 - age;
+
+console.log(getRemainingYears(15));
