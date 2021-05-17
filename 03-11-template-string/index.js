846436389ebce575ca5a9ab5b094e14228f8b46f
RobertoScolar <robertobs1@hotmail.com>
2021-05-17 16:15:40 -0300

    template strings

---
+++ 03-11-template-string/index.js
@@ -0,0 +1,11 @@
+const nome = 'Lucas';
+const idade = 32;
+
+const sentence1 = 'Meu nome é ' + nome + ' e eu tenho ' + idade + ' anos.';
+
+//sentence2 também respeita quebra de linha escrita no código.
+const sentence2 = `Meu nome é ${nome} e eu tenho ${idade} anos.`;
+
+console.log(sentence1);
+console.log(sentence2)
+
