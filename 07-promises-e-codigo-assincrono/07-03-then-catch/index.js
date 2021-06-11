7cd6aa53b5f71e00e75553659aae1ab60b01e5c7
RobertoScolar <robertobs1@hotmail.com>
2021-06-11 02:14:10 -0300

    then e catch

--- 07-promises-e-codigo-assincrono/07-03-then-catch/index.js
+++ 07-promises-e-codigo-assincrono/07-03-then-catch/index.js
@@ -3,3 +3,26 @@ const promise = new Promise((resolve, reject) => {
     resolve('123')
   }, 5000);
 })
+
+//encapsulando as promises dentro de uma função
+
+function handleClick () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('123')
+    }, 5000);
+  })
+}
+
+//para acessar o valor de uma promise precisamos usar o método then
+//o valor do retorno de then será o valor final da promise
+//o código só irá ser aplicado se a promise estiver resolvida
+const result = handleClick()
+  .then(res => {
+    console.log(res === 123 )
+    return res
+  })
+  .catch(err => {
+    console.log('houve um erro')
+    console.log(err)
+  })
