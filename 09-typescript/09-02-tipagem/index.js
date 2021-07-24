347a8d4ba0ded5fa56cfc71079452dc1f68cdd01
Roberto Scolar <robertobs1@hotmail.com>
2021-07-24 12:45:11 -0300

    tipagem

---
+++ 09-typescript/09-02-tipagem/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var personName = 'Daniel';
+var personAge = 22;
+var personAlive = true;
+var contacts = ['email.com', '(11)151515156'];
+var contacts2 = ['skaslskd@gmail.com', '115186'];
---
+++ 09-typescript/09-02-tipagem/index.ts
@@ -0,0 +1,6 @@
+const personName:string = 'Daniel';
+const personAge:number = 22;
+const personAlive:boolean = true;
+
+const contacts:string[] = ['email.com','(11)151515156']
+const contacts2:(string|number)[] = ['skaslskd@gmail.com','115186']
