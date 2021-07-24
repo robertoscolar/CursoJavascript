218ce621bc762e81e21de3517e44183e5de49f68
Roberto Scolar <robertobs1@hotmail.com>
2021-07-24 18:30:44 -0300

    import e export

---
+++ 09-typescript/09-08-exportando-importando-interfaces-types/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//import { Person,Email } from "./interfaces";
+//interface.ts e Person.d.ts estão fazendo o mesmo papel
+//na hora de aplicar só um é necessário
+var person = {
+    name: 'Lucas',
+    age: 32
+};
+var email = 'Daniel';
---
+++ 09-typescript/09-08-exportando-importando-interfaces-types/index.ts
@@ -0,0 +1,11 @@
+//import { Person,Email } from "./interfaces";
+
+//interface.ts e Person.d.ts estão fazendo o mesmo papel
+//na hora de aplicar só um é necessário
+
+const person:Person.Default= {
+    name:'Lucas',
+    age:32
+}
+
+const email:Person.Email = 'Daniel'
